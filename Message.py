8945546bd40c68317d66e327b39329a8b555522d
Scarlehoff <jumax9.gently@gmail.com>
2016-12-18 00:53:44 +0100

    refactoring UpdateUtil into Message and ProcessUpdate

---
+++ Message.py
@@ -0,0 +1,78 @@
+# The only reason for this class to exists is Order and Organisation
+
+class Message:
+# Variables that we are going to parse from json:
+# chatId              - Id of the chat the message came from
+# username            - user who sent the message
+# isCommand           - t/f
+# isRegisteredCommand - t/f
+# isGroup             - t/f
+# isFile              - t/f
+# fileId              - file id
+# text                - actual content of the message (minus /command)
+# command             - command given in /command (or None)
+# ignore              - t/f (whether this message should be ignored)
+
+    def __init__(self, jsonDict):
+        # Global variables
+        registeredCommands = ["poder", "habilidad", "store"]
+        # ignore keys:
+        ik1 = "new_chat_participant"
+        ik2 = "left_chat_participant"
+        msg = "message"
+
+        keys = jsonDict.keys()
+        if msg not in keys:
+            msg = "edited message"
+        try:
+            message = jsonDict[msg]
+        except:
+            print("   >>>>> ")
+            print(jsonDict)
+            raise Exception("Not a message or an edited message?")
+        msgKeys  = message.keys()
+        if ik1 in msgKeys or ik2 in msgKeys:
+            self.ignore = True
+            return
+        else:
+            self.ignore = False
+        fromData = message['from']
+        chatData = message['chat']
+        # Populate general fields
+        self.username = fromData['username']
+        self.chatId   = chatData['id']
+        if "text"  in msgKeys:
+            self.text = message["text"]
+        elif "caption" in msgKeys:
+            self.text = message["caption"]
+        if chatData['type'] == 'group':
+            self.isGroup = True
+        else:
+            self.isGroup = False
+        if self.text[0] == '/':
+            self.isCommand = True
+        else:
+            self.isCommand           = False
+            self.command             = None
+            self.isRegisteredCommand = False
+        if self.isCommand:
+            allText = self.text.split(' ', 1)
+            self.command = allText[0][1:]
+            self.text    = allText[-1]
+            if self.command in registeredCommands:
+                self.isRegisteredCommand = True
+            else:
+                self.isRegisteredCommand = False
+        if "photo" in msgKeys:
+            self.isFile = True
+            photoData   = message["photo"][-1]
+            self.fileId = photoData["file_id"]
+        else:
+            self.isFile = False
+
+
+        
+
+
+
+
---
+++ ProcessUpdate.py
@@ -0,0 +1,85 @@
+class ProcessUpdate:
+    def __init__(self, update, db, telegram):
+        self.update   = update   # Message Class
+        self.db       = db       # basedatos Class
+        self.telegram = telegram # TelegramUtil Clss
+        self.response = None
+        if self.update.isCommand:
+            command = self.update.command
+        else:
+            self.__sendHelp()
+            return
+        habilidad = "habilidad"
+        poder     = "poder"
+        store     = "store"
+        if command == "habilidad":
+            self.__printHabilidad()
+        elif command == "poder":
+            self.__printPoder()
+        elif command == "store":
+            self.__storeInfo()
+        else:
+            self.__printError("Command not recognised")
+
+    # Wrappers for the Telegram class
+    def __sendMessage(self, text):
+        self.telegram.sendMessage(text, self.update.chatId)
+    def __sendImage(self, imgPath):
+        self.telegram.sendImage(imgPath, self.update.chatId)
+
+    def __sendHelp(self):
+        helpmsg = "This is"
+        
+    def __saveImg(self):
+        fileUrl  = self.telegram.getFilePath(self.update.fileId)
+        from urllib import request
+        filename = "img/" + self.update.fileId
+        resource = request.urlretrieve(fileUrl, filename)
+        return filename
+
+    def __printHabilidad(self):
+        args = self.update.text.split(' ') # should be "personaje caracteristica"
+        print(args)
+        if len(args) == 2:
+            info = self.db.readTable("habilidad",["personaje","caracteristica"],args,"poder")
+            if len(info) > 0:
+                response = "The answer is: "
+                for i in info: response += i[0]
+            else:
+                self.__printError("Could not find this value in the database, I'm so so sorry")
+                return
+        else: 
+            self.__printError("Illo, es /habilidad personaje caracteristica, no me lies")
+            return
+        self.__sendMessage(response)
+
+    def __printPoder(self):
+        imgpaths = self.db.readTable("poder", "nombre", self.update.text, "imgpath")
+        if len(imgpaths) > 0:
+            for i in imgpaths:
+                imagePath = i[0] #if there's more than one, send the last one!
+        else:
+           self.__printError("Can't find it")
+           return
+        self.__sendImage(imagePath)
+
+    def __storeInfo(self):
+        args = self.update.text.split(' ')
+        if args[0] == "habilidad" and len(args) == 4:
+            self.db.insertDataInTable(args[1:], "habilidad")
+        elif args[0] == "poder":
+            if self.update.isFile:
+                imgPath = self.__saveImg()
+                imgName = self.update.text.split(' ', 1)[-1]
+                self.db.insertDataInTable([imgName, imgPath], "poder")
+            else:
+                self.__printError("Necesito una imagen para el poder tiiio!")
+                return
+        else:
+            self.__printError("Necesitas mas argumentos para esto cabesa")
+            return
+        self.__sendMessage("Oido cocina!")
+
+    def __printError(self, errorMsg):
+        self.__sendMessage("Error: " + errorMsg)
+        
